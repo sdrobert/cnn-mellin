75a52504d9f7923ae6efba8f71f9b338b2363a76
Sean Robertson <sdrobert@cs.toronto.edu>
2018-09-21 19:45:38 -0400

    Added train_am_for_epoch, test

--- cnn_mellin/running.py
+++ cnn_mellin/running.py
@@ -25,10 +25,15 @@ def get_am_alignment_cross_entropy(
     weight : FloatTensor, optional
         Relative weights to assign to each class. If unset, weights are
         uniform
+
+    Returns
+    -------
+    average_loss : float
+        The cross-entropy loss averaged over all context windows
     '''
     if not len(data_loader):
         raise ValueError('There must be at least one batch of data!')
-    total_frames = 0
+    total_windows = 0
     total_loss = 0
     loss_fn = torch.nn.CrossEntropyLoss(weight=weight, reduction='sum')
     if cuda:
@@ -36,7 +41,7 @@ def get_am_alignment_cross_entropy(
     model.eval()
     with torch.no_grad():
         for feats, ali, feat_sizes, _ in data_loader:
-            total_frames += sum(feat_sizes)
+            total_windows += sum(feat_sizes)
             if ali is None:
                 raise ValueError('Alignments must be specified!')
             if cuda:
@@ -44,4 +49,54 @@ def get_am_alignment_cross_entropy(
                 ali = ali.cuda()
             pdfs = model(feats)
             total_loss += loss_fn(pdfs, ali).item()
-    return total_loss / total_frames
+    return total_loss / total_windows
+
+
+def train_am_for_epoch(
+        model, data_loader, optimizer, params,
+        epoch=None, cuda=False, weight=None):
+    '''Train an acoustic model for one epoch using cross-entropy loss
+
+    Parameters
+    ----------
+    model : AcousticModel
+    data_loader : TrainingDataLoader
+    epoch : int, optional
+        The epoch we are running. If unset, does not touch `data_loader`'s
+        epoch
+    cuda : bool, optional
+        Whether to move the model/data to the GPU
+    weight : FloatTensor, optional
+        Relative weights to assign to each class. If unset, weights are
+        uniform
+
+    Returns
+    -------
+    running_loss : float
+        The batch-averaged cross-entropy loss for the epoch
+    '''
+    epoch_loss = 0.
+    total_batches = 0
+    if epoch is None:
+        epoch = data_loader.epoch
+    else:
+        data_loader.epoch = epoch
+    if cuda:
+        model.cuda()
+    model.train()
+    if params.seed is not None:
+        torch.manual_seed(params.seed + epoch)
+    model.dropout = params.dropout_prob
+    loss_fn = torch.nn.CrossEntropyLoss(weight=weight)
+    for feats, ali in data_loader:
+        if cuda:
+            feats = feats.cuda()
+            ali = ali.cuda()
+        optimizer.zero_grad()
+        pdfs = model(feats)
+        loss = loss_fn(pdfs, ali)
+        loss.backward()
+        optimizer.step()
+        epoch_loss += loss.item()
+        total_batches += 1
+    return epoch_loss / total_batches
--- tests/test_running.py
+++ tests/test_running.py
@@ -15,13 +15,28 @@ __copyright__ = "Copyright 2018 Sean Robertson"
 
 
 class DummyAM(torch.nn.Module):
-    def __init__(self, num_filts, num_classes):
+    def __init__(self, num_filts, num_classes, seed=1):
         super(DummyAM, self).__init__()
+        self.seed = seed
+        torch.manual_seed(seed)
         self.fc = torch.nn.Linear(num_filts, num_classes)
+        self.drop = torch.nn.Dropout(p=0.5)
 
     def forward(self, x):
         x = self.fc(x)
         return x.sum(1)  # sum out the context window
+
+    def reset_parameters(self):
+        torch.manual_seed(self.seed)
+        self.fc.reset_parameters()
+
+    @property
+    def dropout(self):
+        return self.drop.p
+
+    @dropout.setter
+    def dropout(self, p):
+        self.drop.p = p
 
 
 def test_get_am_alignment_cross_entropy(temp_dir, device, populate_torch_dir):
@@ -41,3 +56,34 @@ def test_get_am_alignment_cross_entropy(temp_dir, device, populate_torch_dir):
     loss_b = running.get_am_alignment_cross_entropy(
         model, data_loader, cuda=device == 'cuda')
     assert abs(loss_a - loss_b) < 1e-5
+
+
+def test_train_am_for_epoch(temp_dir, device, populate_torch_dir):
+    populate_torch_dir(temp_dir, 50)
+    spect_p = params.SpectDataSetParams(
+        context_left=1,
+        context_right=1,
+        batch_size=5,
+        seed=2,
+        drop_last=True,
+    )
+    data_loader = data.TrainingDataLoader(temp_dir, spect_p, num_workers=4)
+    train_p = params.TrainingParams(
+        num_epochs=10,
+        seed=3,
+        dropout_prob=.5,
+    )
+    model = DummyAM(5, 11)
+    # important! Use optimizer without history
+    optimizer = torch.optim.SGD(model.parameters(), lr=0.1)
+    loss_a = running.train_am_for_epoch(
+        model, data_loader, optimizer, train_p, cuda=device == 'cuda')
+    assert loss_a != 0
+    loss_b = running.train_am_for_epoch(
+        model, data_loader, optimizer, train_p, cuda=device == 'cuda')
+    assert loss_a > loss_b  # we learned something, maybe?
+    optimizer.zero_grad()
+    model.reset_parameters()
+    loss_c = running.train_am_for_epoch(
+        model, data_loader, optimizer, train_p, epoch=0)
+    assert abs(loss_a - loss_c) < 1e-5
