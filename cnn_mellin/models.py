c04ab7f8b11f69b14755a8611184ef6a9d175759
Sean Robertson <sdrobert@cs.toronto.edu>
2019-01-30 18:29:43 -0500

    Factored conv layer size calculations out of AcousticModel

--- cnn_mellin/models.py
+++ cnn_mellin/models.py
@@ -1,6 +1,8 @@
 '''Acoustic models'''
 
 from itertools import chain
+from functools import reduce
+from operator import mul
 
 import param
 import torch
@@ -110,6 +112,14 @@ class AcousticModelParams(param.Parameterized):
 
 
 class AcousticModel(torch.nn.Module):
+    '''The acoustic model
+
+    Parameters
+    ----------
+    params : AcousticModelParams
+    window : int
+        The total size of the window in time (context_left + context_right + 1)
+    '''
 
     def __init__(self, params, window):
         super(AcousticModel, self).__init__()
@@ -120,10 +130,6 @@ class AcousticModel(torch.nn.Module):
         self.nonlins = torch.nn.ModuleList([])
         self.drops = torch.nn.ModuleList([])
         self._drop_p = 0.0
-        prev_w = window
-        prev_h = params.freq_dim
-        if prev_h is None:
-            raise ValueError('freq_dim must be set!')
         if params.target_dim is None:
             raise ValueError('target_dim must be set!')
         if params.nonlinearity == 'relu':
@@ -132,109 +138,21 @@ class AcousticModel(torch.nn.Module):
             Nonlin = torch.nn.Sigmoid
         else:
             Nonlin = torch.nn.Tanh
-        if params.num_conv:
-            prev_chan = 1
-            decim_w = params.time_factor
-            decim_h = params.freq_factor
-            if params.channels_factor is not None:
-                mult_chan = params.channels_factor
-            else:
-                mult_chan = decim_w * decim_h
-            kw = params.kernel_time
-            kh = params.kernel_freq
-            dh = 1
-            ph = kh // 2
-            if params.factor_sched is None:
-                sched = float('inf')
-            else:
-                sched = params.factor_sched + 1
-            if params.dropout2d_on_conv:
-                Dropout = torch.nn.Dropout2D
-            else:
-                Dropout = torch.nn.Dropout
-            if params.mellin:
-                from pydrobert.mellin.torch import (
-                    MellinLinearCorrelation as Conv)
-                off_pw = kw - 1
-                off_dw = 1
-                if params.mconv_decimation_strategy == 'pad-to-dec-time-ceil':
-                    on_pw = (kw - 1) // decim_w
-                    on_dw = (on_pw + 1) * decim_w - kw + 1
-                elif params.mconv_decimation_strategy == 'pad-then-dec':
-                    on_pw = kw - 1
-                    on_dw = (decim_w - 1) * kw + 1
-                else:
-                    on_pw = max((kw - decim_w) // decim_w, 0)
-                    on_dw = max((on_pw + 1) * decim_w - kw + 1, 1)
-
-                def _update_size_and_kwargs(on):
-                    if on:
-                        pw, dw = on_pw, on_dw
-                        cur_w = (prev_w + decim_w - 1) // decim_w
-                        cur_h = (prev_h + decim_h - 1) // decim_h
-                    else:
-                        pw, dw = off_pw, off_dw
-                        cur_w = prev_w
-                        cur_h = prev_h
-                    sw = 1
-                    rw = cur_w - ((pw + 1) * prev_w - 1) // (kw + dw - 1) - 1
-                    sh = (prev_h + - (dh - 1) * (kh - 1) - 1) // cur_h + 1
-                    rh = cur_h - (prev_h + ph - dh * (kh - 1) - 1) // sh - 1
-                    return cur_w, cur_h, {
-                        'p': (pw, ph),
-                        's': (sw, sh),
-                        'd': (dw, dh),
-                        'r': (rw, rh),
-                    }
-            else:
-                Conv = torch.nn.Conv2d
-                pw = kw // 2
-                dw = 1
-
-                def _update_size_and_kwargs(on):
-                    if on:
-                        cur_w = (prev_w + decim_w - 1) // decim_w
-                        cur_h = (prev_h + decim_h - 1) // decim_h
-                    else:
-                        cur_w = prev_w
-                        cur_h = prev_h
-                    sh = (prev_h + 2 * ph - dh * (kh - 1) - 1) // cur_h + 1
-                    cur_h = (prev_h + 2 * ph - dh * (kh - 1) - 1) // sh + 1
-                    sw = (prev_w + 2 * pw - dw * (kw - 1) - 1) // cur_w + 1
-                    cur_w = (prev_w + 2 * pw - dw * (kw - 1) - 1) // sw + 1
-                    return cur_w, cur_h, {
-                        'padding': (pw, ph),
-                        'stride': (sw, sh),
-                        'dilation': (dw, dh),
-                    }
-            for layer_idx in range(params.num_conv):
-                sched -= 1
-                if not sched:
-                    sched = params.factor_sched
-                    on = True
-                    cur_chan = mult_chan * prev_chan
-                else:
-                    on = False
-                    cur_chan = prev_chan
-                if not layer_idx:
-                    cur_chan = params.init_num_channels
-                if (
-                        layer_idx == params.num_conv - 1 and
-                        params.flatten_style == 'keep_filts'):
-                    cur_chan = 1
-                cur_w, cur_h, kwargs = _update_size_and_kwargs(on)
-                self.convs.append(Conv(
-                    prev_chan, cur_chan, (kw, kh),
-                    **kwargs
-                ))
-                self.nonlins.append(Nonlin())
-                self.drops.append(Dropout(p=0.0))
-                prev_chan, prev_w, prev_h = cur_chan, cur_w, cur_h
-            if params.flatten_style == 'keep_chans':
-                prev_h = prev_chan
-            elif params.flatten_style == 'keep_both':
-                prev_h *= prev_chan
-        prev_size = prev_w * prev_h
+        if params.dropout2d_on_conv:
+            Dropout = torch.nn.Dropout2D
+        else:
+            Dropout = torch.nn.Dropout
+        conv_config = _get_conv_config(params, window)
+        for layer_config in conv_config['layers']:
+            self.convs.append(conv_config['class'](
+                layer_config['in_chan'],
+                layer_config['out_chan'],
+                (layer_config['kw'], layer_config['kh']),
+                **layer_config['kwargs']
+            ))
+            self.nonlins.append(Nonlin())
+            self.drops.append(Dropout(p=0.0))
+        prev_size = conv_config['out_size']
         for layer_idx in range(params.num_fc):
             if layer_idx == params.num_fc - 1:
                 self.fcs.append(torch.nn.Linear(prev_size, params.target_dim))
@@ -278,3 +196,118 @@ class AcousticModel(torch.nn.Module):
         for layer in self.drops:
             layer.p = p
         self._drop_p = p
+
+
+def _get_conv_config(params, window):
+    config = dict()
+    config['layers'] = layers = []
+    prev_w = window
+    prev_h = params.freq_dim
+    if prev_h is None:
+        raise ValueError('freq_dim must be set!')
+    if not params.num_conv:
+        config['out_size'] = prev_w * prev_h
+        return config
+    prev_chan = 1
+    decim_w = params.time_factor
+    decim_h = params.freq_factor
+    if params.channels_factor is not None:
+        mult_chan = params.channels_factor
+    else:
+        mult_chan = decim_w * decim_h
+    kw = params.kernel_time
+    kh = params.kernel_freq
+    dh = 1
+    ph = kh // 2
+    if params.factor_sched is None:
+        sched = float('inf')
+    else:
+        sched = params.factor_sched + 1
+    if params.mellin:
+        from pydrobert.mellin.torch import MellinLinearCorrelation
+        config['class'] = MellinLinearCorrelation
+        off_pw = kw - 1
+        off_dw = 1
+        if params.mconv_decimation_strategy == 'pad-to-dec-time-ceil':
+            on_pw = (kw - 1) // decim_w
+            on_dw = (on_pw + 1) * decim_w - kw + 1
+        elif params.mconv_decimation_strategy == 'pad-then-dec':
+            on_pw = kw - 1
+            on_dw = (decim_w - 1) * kw + 1
+        else:
+            on_pw = max((kw - decim_w) // decim_w, 0)
+            on_dw = max((on_pw + 1) * decim_w - kw + 1, 1)
+
+        def _update_size_and_kwargs(on):
+            if on:
+                pw, dw = on_pw, on_dw
+                cur_w = (prev_w + decim_w - 1) // decim_w
+                cur_h = (prev_h + decim_h - 1) // decim_h
+            else:
+                pw, dw = off_pw, off_dw
+                cur_w = prev_w
+                cur_h = prev_h
+            sw = 1
+            rw = cur_w - ((pw + 1) * prev_w - 1) // (kw + dw - 1) - 1
+            sh = (prev_h + - (dh - 1) * (kh - 1) - 1) // cur_h + 1
+            rh = cur_h - (prev_h + ph - dh * (kh - 1) - 1) // sh - 1
+            return cur_w, cur_h, {
+                'p': (pw, ph),
+                's': (sw, sh),
+                'd': (dw, dh),
+                'r': (rw, rh),
+            }
+    else:
+        config['class'] = torch.nn.Conv2d
+        pw = kw // 2
+        dw = 1
+
+        def _update_size_and_kwargs(on):
+            if on:
+                cur_w = (prev_w + decim_w - 1) // decim_w
+                cur_h = (prev_h + decim_h - 1) // decim_h
+            else:
+                cur_w = prev_w
+                cur_h = prev_h
+            sh = (prev_h + 2 * ph - dh * (kh - 1) - 1) // cur_h + 1
+            cur_h = (prev_h + 2 * ph - dh * (kh - 1) - 1) // sh + 1
+            sw = (prev_w + 2 * pw - dw * (kw - 1) - 1) // cur_w + 1
+            cur_w = (prev_w + 2 * pw - dw * (kw - 1) - 1) // sw + 1
+            return cur_w, cur_h, {
+                'padding': (pw, ph),
+                'stride': (sw, sh),
+                'dilation': (dw, dh),
+            }
+    for layer_idx in range(params.num_conv):
+        sched -= 1
+        if not sched:
+            sched = params.factor_sched
+            on = True
+            cur_chan = mult_chan * prev_chan
+        else:
+            on = False
+            cur_chan = prev_chan
+        if not layer_idx:
+            cur_chan = params.init_num_channels
+        if (
+                layer_idx == params.num_conv - 1 and
+                params.flatten_style == 'keep_filts'):
+            cur_chan = 1
+        cur_w, cur_h, kwargs = _update_size_and_kwargs(on)
+        entry = {
+            'in_chan': prev_chan,
+            'out_chan': cur_chan,
+            'kw': kw,
+            'kh': kh,
+            'out_w': cur_w,
+            'out_h': cur_h,
+            'kwargs': kwargs
+        }
+        layers.append(entry)
+        prev_chan, prev_w, prev_h = cur_chan, cur_w, cur_h
+    if params.flatten_style == 'keep_chans':
+        prev_h = prev_chan
+    elif params.flatten_style == 'keep_both':
+        prev_h *= prev_chan
+    config['out_size'] = prev_w * prev_h
+    return config
